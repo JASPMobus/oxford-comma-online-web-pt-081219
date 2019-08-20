a58bb35c09e50d2f66652ac31f7cf121f205ecc2
James Mobus <mobusj@live.com>
2019-08-20 23:22:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,9 @@ def oxford_comma(array)
   when 2
     "#{array[0]} and #{array[1]}"
   else
-    array.inject do |
+    last = array.last
+    
+    array.pop.inject do |ret, item|
+      
+    end
 end
